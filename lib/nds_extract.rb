4d23f79d8068dfd36f370a73e9051ddf1a160e20
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-18 21:57:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,14 +22,3 @@ def directors_totals(nds)
   end
   total
 end
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
-end
